1d5ebc8ae08023b27c8d4b6bceba73d3f0bbe62d
ElVeexo <vicente25852@gmail.com>
2026-03-26 20:13:28 -0300

    chore: push inicial del frontend con CI/CD

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className="container">
+      <h1>ReturnIQ Front</h1>
+      <p>Frontend React ultra sencillo funcionando.</p>
+      <button onClick={() => alert('Hola desde React')}>Probar</button>
+    </main>
+  );
+}
+
+export default App;
